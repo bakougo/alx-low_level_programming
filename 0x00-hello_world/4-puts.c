50b5ebbdaaa36b2c44abbccc5cac4677e28ddb01
Abel <abelasfaw000@gmail.com>
2022-06-02 14:02:56 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - Entry point
+ *
+ * puts: "Programming is like building a multilingual puzzel." new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*Entered strings */
+
 	char str[] = "Programming is like building a multilingual puzzel.\n";
 
-	/*writting to standard output */
+
 	puts(str);
 
 	return (0);
