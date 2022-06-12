5ba7edbd5b9c69d0f6e74794c0819b1db33cf3b0
Abel <abelasfaw000@gmail.com>
2022-06-12 09:08:17 -0700

    jack B

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		for (M = 0; M < 60; H++)
 		{
-			_putchar((H / 10) + '0');
-			_putchar((H % 10) + '0');
-			_putchar(':');
 			_putchar((M / 10) + '0');
 			_putchar((M % 10) + '0');
+			_putchar(':');
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
 			_putchar('\n');
 		}
 	}
