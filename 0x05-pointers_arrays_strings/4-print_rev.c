c8039bc5800b7df127b251e641ba7fc1d7a36650
Abel <abelasfaw000@gmail.com>
2022-06-13 15:18:35 -0700

    vomisa

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,15 @@
  */
 void print_rev(char *s)
 {
-	char *len = s;
+	int i = 0;
 
-	while (*len)
-		len++;
-	len -= 1;
-
-	while (len)
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
 	{
-		_putchar(*s);
-		len--;
+		_putchar(*(s + i));
+		i--;
 	}
 	_putchar('\n');
 }
