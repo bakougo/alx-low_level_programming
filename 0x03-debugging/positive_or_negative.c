a678b73c0367b8c15b6ac5c7171e0646b79b5d82
Abel <abelasfaw000@gmail.com>
2022-06-15 10:32:19 -0700

    pos -ve

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include "main.h"
 /**
  *main - Entry point
  *
@@ -9,8 +9,8 @@
  *
  */
 
-/* betty style doc for function positive_or_negative */
-int positive_or_negative(void)
+
+int main(void)
 {
 	int n;
 
