fb1adee815e550eb09d31a824da77fd1419f0eb4
Abel <abelasfaw000@gmail.com>
2022-07-02 09:09:50 -0700

    its leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - capitalize all words of a string
+ * @s: pointer to string
+ * Return: the pointer to the string
+ */
+char *leet(char *s)
+{
+	int c, j;
+	char letter[12] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char number[12] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' ||
+		s[c] == 'o' || s[c] == 'O' || s[c] == 't' || s[c] == 'T' ||
+		s[c] == '1' || s[c] == 'L')
+	{
+		for (j = 0; s[c] != letter[j]; j++)
+		{}
+		s[c] = number[j];
+	}
+	}
+	return (s);
+}
